eff4e811d7a674ef15833916efc4a539208de3da
vitor de oliveira <vitoroliveira732@gmail.com>
2022-02-27 22:24:14 -0300

    main

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import styles from './Home.module.css'
 import { useState, useEffect } from 'react'
+import Loading from '../layout/Loading'
 
 function Content() {
   const [pokemons, setPokemons] = useState([])
   const [nextPokemons, setNextPokemons] = useState()
   const [previousPokemons, setPreviousPokemons] = useState()
+  const [removeLoading, setRemoveLoading] = useState(true)
 
   const options = {
     method: 'GET',
@@ -12,7 +14,6 @@ function Content() {
       'Content-Type': 'application/json',
     },
   }
-
 
   const loadingPokemons =  () => {
     fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=15", options)
@@ -22,17 +23,12 @@ function Content() {
         setPokemons(data.results)
         setNextPokemons(data.next)
         setPreviousPokemons(data.previous)
-
       })
-
-
   }
-
 
   useEffect(() => {
     loadingPokemons()
   }, [])
-
 
 
   const clearPokemons = () => {
@@ -43,57 +39,63 @@ function Content() {
 
   const nextLoading =  () => {
     clearPokemons()
+    setRemoveLoading(false)
 
-    fetch(`${nextPokemons}`, options)
-      .then(response => {
-        return response.json()
-      }).then((data) => {
-        setPokemons(data.results)
-        setNextPokemons(data.next)
-        setPreviousPokemons(data.previous)
-      })
+    setTimeout(
+      fetch(`${nextPokemons}`, options)
+        .then(response => {
+          return response.json()
+        }).then((data) => {
+          setPokemons(data.results)
+          setNextPokemons(data.next)
+          setPreviousPokemons(data.previous)
+          setRemoveLoading(true)
+        }),
+      8,
+      )
 
   }
 
   const previousLoading = () => {
     clearPokemons()
 
-    fetch(`${previousPokemons}`, options)
-      .then(response => {
-        return response.json()
-      }).then((data) => {
-        setPokemons(data.results)
-        setNextPokemons(data.next)
-        setPreviousPokemons(data.previous)
-      })
-
+      fetch(`${previousPokemons}`, options)
+        .then(response => {
+          return response.json()
+        }).then((data) => {
+          setPokemons(data.results)
+          setNextPokemons(data.next)
+          setPreviousPokemons(data.previous)
+          
+        })
   }
 
-
-
-
   return (
-    <div className={styles.Content}>
-      <ul>
-        {pokemons.map(({ name }) => (
-          <div className={styles.Pokemon}>
-            <img src={`https://img.pokemondb.net/artwork/large/${name}.jpg`} />
-            <li key={name} >{name}</li>
+    <div>
+    {!removeLoading ? (<Loading />
+    ):(removeLoading && <div className={styles.Content}>
+          <ul>
+            {pokemons.map(({ name }) => (
+              <div className={styles.Pokemon}>
+                <img src={`https://img.pokemondb.net/artwork/large/${name}.jpg`} />
+                <li key={name} >{name}</li>
+              </div>
+            ))
+            }
+          </ul>
+
+          <div className={styles.Button}>
+            <button onClick={previousLoading} >
+              Anterior
+            </button>
+
+            <button onClick={nextLoading} >
+              Proximo
+            </button>
+        
           </div>
-        ))
-        }
-      </ul>
-
-      <div className={styles.Button}>
-        <button onClick={previousLoading} >
-          Anterior
-        </button>
-
-        <button onClick={nextLoading} >
-          Proximo
-        </button>
-
-      </div>
+        </div>
+      )}
     </div>
   )
 }
